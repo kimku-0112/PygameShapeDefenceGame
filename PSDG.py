5d4253dc1bdc0d05285602e60a70a9b7e7b3de85
kimku-0112 <kimku5378@gmail.com>
2020-06-14 22:24:09 +0900

    update : 대소문자업데이트

--- PSDG.py
+++ PSDG.py
@@ -2,80 +2,80 @@ import pygame, sys, random
 from pygame.locals import *
 
 FPS             = 60            #┐
-WINDOWWIDTH     = 640           #│Define System Information 
-WINDOWHEIGHT    = 480           #┘
+window_width    = 640          #│
+window_height   = 480          #│ Define System Information 
+reveal_speed    = 8            #│ 
+gap_size        = 0            #│
+box_size        = 40            #┘
 
-GRAY        = (100,100,100)     #┐
-NAVYBLUE    = (60,60,100)       #│
-WHITE       = (255,255,255)     #│
-RED         = (255,0,0)         #│
-GREEN       = (0,255,0)         #│ Define Color
-BLUE        = (0,0,255)         #│
-YELLOW      = (255,255,0)       #│
-ORANGE      = (255,128,0)       #│
-PURPLE      = (255,0,255)       #┘
-CYAN        = (0,255,255)       
+gray            = (100,100,100)     #┐
+navyblue        = (60,60,100)       #│
+white           = (255,255,255)     #│
+red             = (255,0,0)         #│
+green           = (0,255,0)         #│ Define Color
+blue            = (0,0,255)         #│
+yellow          = (255,255,0)       #│
+orange          = (255,128,0)       #│
+purple          = (255,0,255)       #┘
+cyan            = (0,255,255)       
 
-BGCOLOR         = NAVYBLUE      #┐
-LIGHTBGCOLOR    = GRAY          #│ System Color
-BOXCOLOR        = WHITE         #│
-HIGHLIGHTCOLOR  = BLUE          #┘
+BG_color        = green         #┐
+light_color     = gray          #│ System Color
+box_color       = white         #│
+highlight_color = blue          #┘
 
-Tower_Color     = RED           #┐
-Enemy_Color     = GRAY          #│
-Road_Color      = GRAY          #│
-Ground_Color    = GREEN         #│ Object Color
-Shop_Color      = WHITE         #│
-Gold_Color      = YELLOW        #│
-Attack_Up_Color = RED           #│
-Delay_Up_Color  = BLUE          #┘
+tower_color     = red           #┐
+enemy_color     = gray          #│
+road_color      = gray          #│
+shop_color      = white         #│ Object Color
+gold_color      = yellow        #│
+attack_up_color = red           #│
+delay_up_color  = blue          #┘
 
-SQUARE          = 'square'      #┐
-TRIANGLE        = 'triangle'    #│ Define Shape 
-PENTAGON        = 'pentagon'    #┘
+square          = 'square'      #┐
+triangle        = 'triangle'    #│ Define Shape 
+pentagon        = 'pentagon'    #┘
 
-REVEALSPEED     = 8              
-BOXSIZE         = 40            
-GAPSIZE         = 0             
-BOARDWIDTH      = 16            
-BOARDHEIGHT     = 11             
-ShopWidth       = WINDOWWIDTH
-ShopHeight      = 40
+map_width       = int(window_width  / box_size)-1
+map_heghit      = int(window_height / box_size)-1
+shop_x          = 0
+shop_y          = 0
+shop_width      = window_width
+shop_height     = window_height - (map_heghit * box_size)
 
-ShopSize        = [[0,0],[0,ShopWidth],[ShopHeight,ShopWidth],[ShopHeight,0]]   #Size
-
-XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH  * (BOXSIZE + GAPSIZE))) / 2)
-YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT  * (BOXSIZE + GAPSIZE))) / 2)
-
-def getBoxAtPixel(x, y):
-    for boxx in range(BOARDWIDTH):
-        for boxy in range(BOARDHEIGHT):
+def GetBoxAtPixel(x, y):
+    for boxx in range(map_width):
+        for boxy in range(map_heghit):
             left, top = leftTopCoordsOfBox(boxx, boxy)
-            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            boxRect = pygame.Rect(left, top, box_size, box_size)
             if boxRect.collidepoint(x, y):
                 return (boxx, boxy)
     return (None, None)
     
-def makemap():
+def MakeMap():
+    display_surf.fill(BG_color)
     
     
-def makeshop():
-    pygame.draw.rect(DISPLAYSURF,Shop_Color,
+def MakeShop():
+    pygame.draw.rect(display_surf,shop_color,(shop_x,shop_y,shop_width,shop_height))
 
 
 def main():
-    global FPSCLOCK, DISPLAYSURF
+    global FPS_clock,display_surf
     pygame.init()
-    FPSCLOCK    = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT ))
-    DISPLAYSURF.fill(BGCOLOR)
-    
-    mousex      = 0
-    mousey      = 0
-    
+    FPS_clock    = pygame.time.Clock()
+    display_surf = pygame.display.set_mode((window_width,window_height ))
     pygame.display.set_caption('Shape Defence Game')
     
+    mouse_x     = 0
+    mouse_y     = 0
     
+    while True:
+        MakeMap()
+        MakeShop()
+    
+        pygame.display.update()
+        FPS_clock.tick(FPS)
     
     
     
